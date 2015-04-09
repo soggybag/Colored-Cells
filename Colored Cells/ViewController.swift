a5591ae699387c41723e159925c3c6abcb85ba62
Mitchell Hudson <soggybag@gmail.com>
2015-04-08 21:12:34 -0700

    initial commit

--- Colored Cells/ViewController.swift
+++ Colored Cells/ViewController.swift
@@ -8,7 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var numberOfRows = 100
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberOfRows
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        cell.textLabel?.text = "Cell \(indexPath.row)"
+        
+        let n = 1.0 / CGFloat(numberOfRows)
+        let hue = CGFloat(indexPath.row) * n
+        let color = UIColor(hue: hue, saturation: 1, brightness: 1, alpha: 1)
+        cell.backgroundColor = color
+        
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
